fd093fa2d5cbba77e2e246f150f3d519f6e8097d
Michele Spinelli <spinellimichele91@protonmail.com>
2019-07-12 02:51:08 +0200

    setup basic structure of addEventListener

---
+++ js/main.js
@@ -0,0 +1,83 @@
+
+document.body.addEventListener("click", function(e) {
+    let clicked = e.target.classList[1];
+    switch(clicked) {
+        case "btn-ce":
+            console.log(clicked);
+            break;
+        case "btn-percentage":
+            console.log(clicked);
+            break;
+        case "btn-division":
+            console.log(clicked);
+            break;
+        case "btn-seven":
+            console.log(clicked);
+            break;
+        case "btn-eight":
+            console.log(clicked);
+            break;
+        case "btn-nine":
+            console.log(clicked);
+            break;
+        case "btn-multiplication":
+            console.log(clicked);
+            break;
+        case "btn-four":
+            console.log(clicked);
+            break;
+        case "btn-five":
+            console.log(clicked);
+            break; 
+        case "btn-six":
+            console.log(clicked);
+            break;
+        case "btn-minus":
+            console.log(clicked);
+            break;
+        case "btn-one":
+            console.log(clicked);
+            break;
+        case "btn-two":
+            console.log(clicked);
+            break;
+        case "btn-three":
+            console.log(clicked);
+            break;
+        case "btn-plus":
+            console.log(clicked);
+            break;
+        case "btn-zero":
+            console.log(clicked);
+            break;
+        case "btn-dot":
+            console.log(clicked);
+            break;
+        case "btn-equal":
+            console.log(clicked);
+            break;
+        default: 
+            console.log("area clicked wasn't setup");
+            break;
+    }
+});
+
+
+// btn-percentage
+// btn-division
+// btn-seven
+// btn-eight
+// btn-nine
+// btn-multiplication
+// btn-four
+// btn-five
+// btn-six
+// btn-minus
+// btn-one
+// btn-two
+// btn-three
+// btn-plus
+// btn-zero
+// btn-dot
+// btn-equal
+// btn-ce
