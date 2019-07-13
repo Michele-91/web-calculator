ae10de3e456a3b556e284f4432ee35367ec8d9f5
Michele Spinelli <spinellimichele91@protonmail.com>
2019-07-13 23:02:57 +0200

    complete calculator functionality, add real-time numbers display

--- js/main.js
+++ js/main.js
@@ -2,63 +2,101 @@
 
 let num = [];
 let expression = [];
+let el = document.createElement('p');
+let display = document.querySelector(".screen__display");
+
+function numDisplay(content) {
+    display.textContent = num.join('');
+}
+
+function signDisplay(content, result) {
+    if(content === "=") {
+        display.textContent = result;
+    }
+}
+
+function reset() {
+    num = [];
+    expression = [];
+    display.textContent = 0;
+}
+
+
 
 document.body.addEventListener("click", function(e) {
     let clicked = e.target.classList[1];
     switch(clicked) {
         case "btn-ce":
+            reset();
             break;
         case "btn-percentage":
             sign(e.target.textContent);
+            signDisplay(e.target.textContent);
             break;
         case "btn-division":
             sign(e.target.textContent);
+            signDisplay(e.target.textContent);
             break;
         case "btn-seven":
             num.push(e.target.textContent);
+            numDisplay(e.target.textContent);
             break;
         case "btn-eight":
             num.push(e.target.textContent);
+            numDisplay(e.target.textContent);
             break;
         case "btn-nine":
             num.push(e.target.textContent);
+            numDisplay(e.target.textContent);
             break;
         case "btn-multiplication":
             sign(e.target.textContent);
+            signDisplay(e.target.textContent);
             break;
         case "btn-four":
             num.push(e.target.textContent);
+            numDisplay(e.target.textContent);
             break;
         case "btn-five":
             num.push(e.target.textContent);
+            numDisplay(e.target.textContent);
             break; 
         case "btn-six":
             num.push(e.target.textContent);
+            numDisplay(e.target.textContent);
             break;
         case "btn-minus":
             sign(e.target.textContent);
+            signDisplay(e.target.textContent);
             break;
         case "btn-one":
             num.push(e.target.textContent);
+            numDisplay(e.target.textContent);
             break;
         case "btn-two":
             num.push(e.target.textContent);
+            numDisplay(e.target.textContent);
             break;
         case "btn-three":
             num.push(e.target.textContent);
+            numDisplay(e.target.textContent);
             break;
         case "btn-plus":
             sign(e.target.textContent);
+            signDisplay(e.target.textContent);
             break;
         case "btn-zero":
             num.push(e.target.textContent);
+            numDisplay(e.target.textContent);
             break;
         case "btn-dot":
             num.push(e.target.textContent);
+            numDisplay(e.target.textContent);
             break;
         case "btn-equal":
             sign(e.target.textContent);
-            calculateResult(expression);
+            let result = calculateResult(expression);
+            signDisplay(e.target.textContent, result);
             break;
         default: 
             console.dir("area clicked wasn't setup");
@@ -80,17 +118,24 @@ const insertOperands = function(operand, sign) {
 const calculateResult = function(expression) {
     let finalEx = ``;
     for(let val of expression) {
-        finalEx += `${val}`;
+        if(val !== "=") {
+            finalEx += `${val}`;
+        }
     }
-    let result = finalEx.match(/\d+|\+\d+|\-\d+/g)
-        .reduce(function (a, b) {
-            return Number(a) + Number(b)
-        });
+    let result = eval(finalEx);
     console.log(result);
-    // document.querySelector(".screen").innerHTML = number;
+    return result;
 };
 
 
+
+
+
+
+// let result = finalEx.match(/\d+|\+\d+|\-\d+/g)
+//     .reduce(function (a, b) {
+//             return Number(a) + Number(b);
+//     });
 
 // btn-percentage
 // btn-division
